796f61ee379f507031190a5e4fe15f404d434c09
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2011-08-23 15:43:49 -0700

    Removing some noise from the logs

--- jetty-slf4j-mdc-handler/src/main/java/org/eclipse/jetty/examples/logging/MDCHandler.java
+++ jetty-slf4j-mdc-handler/src/main/java/org/eclipse/jetty/examples/logging/MDCHandler.java
@@ -8,27 +8,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.HandlerWrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 public class MDCHandler extends HandlerWrapper
 {
-    private static final Logger LOG = LoggerFactory.getLogger(MDCHandler.class);
-    
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
         try
         {
-            LOG.info("Start");
             MDC.put("host",request.getServerName());
             super.handle(target,baseRequest,request,response);
         }
         finally
         {
             MDC.clear();
-            LOG.info("Finally");
         }
     }
 }
